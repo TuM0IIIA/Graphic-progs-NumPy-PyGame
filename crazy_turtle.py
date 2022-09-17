6686f8a936fb43377a70b0b3a00970539913b4df
TuM0IIIA <Timofey.Posled@gmail.com>
2022-09-17 11:22:58 +0300

    new comment has been added

--- crazy_turtle.py
+++ crazy_turtle.py
@@ -1,10 +1,15 @@
 from random import *
 import random as rand
 import turtle
+
+""" Creation of a chaotic movement of the turtle """
+
+
 def screen(*args):
     drawing_area = turtle.Screen()
     drawing_area.bgcolor('yellow')
     drawing_area.setup(750, 750)
+
 
 screen()
 
@@ -13,12 +18,12 @@ x.shape('turtle')
 x.color('black')
 x.speed(10)
 
+
 def crazy():
     for k in range(1000):
         k = rand.randint(0, 360)
         x.forward(k)
         x.right(k)
 
+
 crazy()
-
-
